1cae333aebcba19cfaf3d229bb5824d1eadc6d4a
Gustavo Rodriguez <gustavo.liccioni@weareadaptive.com>
2025-02-27 20:19:21 +0100

    consent

--- src/Balrock.jsx
+++ src/Balrock.jsx
@@ -1,7 +1,7 @@
 //import React from "react";
 import { motion } from "framer-motion";
 import { FaInstagram, FaYoutube, FaFacebook } from "react-icons/fa";
-import './Balrock.css';
+import "./Balrock.css";
 
 const BASE_URL = import.meta.env.BASE_URL;
 
@@ -15,46 +15,53 @@ const images = [
 ];
 
 const upcomingShows = [
-    { date: "01 de Febrero, 2025", location: "Barcelona, España", venue: "Sala Hangar 05" },
-    { date: "24 de Mayo, 2025", location: "Barcelona, España", venue: "Sala Hangar 05",buyLink:"https://www.eventbrite.com/e/entradas-balrock-ii-hangar-05-1260042293419?aff=oddtdtcreator" },
-  ];
+  {
+    date: "01 de Febrero, 2025",
+    location: "Barcelona, España",
+    venue: "Sala Hangar 05",
+  },
+  {
+    date: "24 de Mayo, 2025",
+    location: "Barcelona, España",
+    venue: "Sala Hangar 05",
+    buyLink:
+      "https://www.eventbrite.com/e/entradas-balrock-ii-hangar-05-1260042293419?aff=oddtdtcreator",
+  },
+];
 
 const videos = [
-    "https://www.youtube-nocookie.com/embed/XE-CbJvGQT4?si=-qUf4StWxmOcMxU3",
-    "https://www.youtube.com/embed/dn-SqeMkZNY?si=RCDUu9zd3abPyg1T",
-    "https://www.youtube.com/embed/5sXMRUrFN2Q?si=xQieVz63MsLKKU1y",
-    "https://www.youtube.com/embed/qUs2-otPVhg?si=kOHwZomy-yQNcwRS",
-  ];
-  
+  "https://www.youtube-nocookie.com/embed/XE-CbJvGQT4?si=-qUf4StWxmOcMxU3",
+  "https://www.youtube.com/embed/dn-SqeMkZNY?si=RCDUu9zd3abPyg1T",
+  "https://www.youtube.com/embed/5sXMRUrFN2Q?si=xQieVz63MsLKKU1y",
+  "https://www.youtube.com/embed/qUs2-otPVhg?si=kOHwZomy-yQNcwRS",
+];
 
 export default function BalrockPage() {
-    return (
-      <div className="bg-black text-gray-200 font-sans">
-        <section
-          className="relative h-screen bg-cover bg-center banner-background">
-          
-          <div className="relative z-10 flex flex-col items-center justify-end h-full">
-            <motion.h1
-              style={{ fontFamily: "'Cinzel', serif" }}
-              className="text-2xl lg:text-8xl md:text-6xl sm:text-4xl font-bold text-red-800 drop-shadow-2xl"
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              &quot;Corred Insensatos...&quot;
-            </motion.h1>
-            <motion.p
-              className="mt-4 text-xl text-gray-300 mb-32 lg:mb-1 md:mb-10 sm:mb-10"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.5, duration: 1 }}
-            >
-              Desatando la furia del inframundo.
-            </motion.p>
-          </div>
-        </section>
-  
-        <section className="py-16 bg-gray-900">
+  return (
+    <div className="bg-black text-gray-200 font-sans">
+      <section className="relative h-screen bg-cover bg-center banner-background">
+        <div className="relative z-10 flex flex-col items-center justify-end h-full">
+          <motion.h1
+            style={{ fontFamily: "'Cinzel', serif" }}
+            className="text-2xl lg:text-8xl md:text-6xl sm:text-4xl font-bold text-red-800 drop-shadow-2xl"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            &quot;Corred Insensatos...&quot;
+          </motion.h1>
+          <motion.p
+            className="mt-4 text-xl text-gray-300 mb-32 lg:mb-1 md:mb-10 sm:mb-10"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            Desatando la furia del inframundo.
+          </motion.p>
+        </div>
+      </section>
+
+      <section className="py-16 bg-gray-900">
         <div className="max-w-5xl mx-auto px-4 overflow-hidden">
           <motion.div
             className="flex flex-nowrap space-x-6"
@@ -74,22 +81,23 @@ export default function BalrockPage() {
           </motion.div>
         </div>
       </section>
-  
-        <section className="py-20 bg-black text-center">
-          <motion.h2
-            className="text-4xl md:text-6xl text-red-500 font-bold"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-          >
-            Siente el Fuego, Escucha el Rugido
-          </motion.h2>
-          <p className="mt-6 text-lg text-gray-400 max-w-2xl mx-auto">
-            Balrock trae la furia del inframundo al escenario con riffs poderosos y tambores estremecedores.
-          </p>
-        </section>
 
-        <section className="py-16 bg-gray-800 text-center">
+      <section className="py-20 bg-black text-center">
+        <motion.h2
+          className="text-4xl md:text-6xl text-red-500 font-bold"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
+        >
+          Siente el Fuego, Escucha el Rugido
+        </motion.h2>
+        <p className="mt-6 text-lg text-gray-400 max-w-2xl mx-auto">
+          Balrock trae la furia del inframundo al escenario con riffs poderosos
+          y tambores estremecedores.
+        </p>
+      </section>
+
+      <section className="py-16 bg-gray-800 text-center">
         <h2 className="text-4xl text-red-500 font-bold">Galería de Videos</h2>
         <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6 max-w-5xl mx-auto px-4">
           {videos.map((video, index) => (
@@ -100,30 +108,40 @@ export default function BalrockPage() {
               transition={{ duration: 0.3 }}
             >
               <iframe
-                className="w-full h-64"
-                src={video}
+                data-cmp-vendor="s30"
+                src="about:blank"
+                className="cmplazyload w-full h-64"
                 title={`Balrock Video ${index + 1}`}
+                width="560"
+                height="315"
+                data-cmp-src={video}
                 frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
               ></iframe>
-             </motion.div>
+            </motion.div>
           ))}
         </div>
       </section>
 
-        <section className="py-16 bg-black-800 text-center">
+      <section className="py-16 bg-black-800 text-center">
         <h2 className="text-4xl text-red-500 font-bold">Próximos Conciertos</h2>
         <div className="mt-8 max-w-3xl mx-auto">
           {upcomingShows.map((show, index) => (
             <div key={index} className="py-4 border-b border-gray-600">
               <p className="text-xl text-gray-300 font-semibold">{show.date}</p>
-              <p className="text-lg text-gray-400">{show.location} - {show.venue}
-              <p>  {show.buyLink &&
-                     <a href={show.buyLink} className="ml-4 text-red-500 hover:underline">
-                     Compra tu entrada
-                   </a>
-                } 
+              <p className="text-lg text-gray-400">
+                {show.location} - {show.venue}
+                <p>
+                  {" "}
+                  {show.buyLink && (
+                    <a
+                      href={show.buyLink}
+                      className="ml-4 text-red-500 hover:underline"
+                    >
+                      Compra tu entrada
+                    </a>
+                  )}
                 </p>
               </p>
             </div>
@@ -131,21 +149,34 @@ export default function BalrockPage() {
         </div>
       </section>
 
-  
-        <footer className="py-10 bg-gray-900 text-center">
-          <div className="flex justify-center space-x-8">
-            <motion.a href="https://www.instagram.com/balrockoficial/" className="text-gray-400 hover:text-red-500 text-3xl" whileHover={{ scale: 1.1 }}>
-              <FaInstagram />
-            </motion.a>
-            <motion.a href="https://www.youtube.com/@BalrockOficial" className="text-gray-400 hover:text-red-500 text-3xl" whileHover={{ scale: 1.1 }}>
-              <FaYoutube />
-            </motion.a>
-            <motion.a href="https://www.facebook.com/balrockband" className="text-gray-400 hover:text-red-500 text-3xl" whileHover={{ scale: 1.1 }}>
-              <FaFacebook />
-            </motion.a>
-          </div>
-          <p className="mt-4 text-gray-500 text-sm">&copy; 2025 Balrock. Todos los derechos reservados.</p>
-        </footer>
-      </div>
-    );
-  }
+      <footer className="py-10 bg-gray-900 text-center">
+        <div className="flex justify-center space-x-8">
+          <motion.a
+            href="https://www.instagram.com/balrockoficial/"
+            className="text-gray-400 hover:text-red-500 text-3xl"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaInstagram />
+          </motion.a>
+          <motion.a
+            href="https://www.youtube.com/@BalrockOficial"
+            className="text-gray-400 hover:text-red-500 text-3xl"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaYoutube />
+          </motion.a>
+          <motion.a
+            href="https://www.facebook.com/balrockband"
+            className="text-gray-400 hover:text-red-500 text-3xl"
+            whileHover={{ scale: 1.1 }}
+          >
+            <FaFacebook />
+          </motion.a>
+        </div>
+        <p className="mt-4 text-gray-500 text-sm">
+          &copy; 2025 Balrock. Todos los derechos reservados.
+        </p>
+      </footer>
+    </div>
+  );
+}
