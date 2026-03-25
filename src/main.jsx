0346e462ea0b9cb21661be1b2c0d7ca6e97204c1
Gustavo Rodriguez Liccioni <liccioni@gmail.com>
2026-03-25 06:33:49 +0100

    Clean up unused scaffold and align config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
 import Balrock from './Balrock.jsx'
 
 createRoot(document.getElementById('root')).render(
